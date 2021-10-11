af480d3fe3444ccb026c33a2021e3b445af51d11
PujitYG <pujityg@gmail.com>
2021-10-12 00:05:14 +0530

    clean up

--- A.java
+++ A.java
@@ -1,19 +1,6 @@
 public class A{
 	public static void main(String[]args){
 		System.out.println("HELLO WORLD");
-		
-//nice
 
-		///HELLOW WWIJWIWIWJIW
-		..
-		/////
-
-
-
-
-
-
-		//kokokokaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-		//okookokoko
 	}
 }
